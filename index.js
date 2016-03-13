0cafabd85b26b14879107d0432abded43da344bf
Henryk Wollik <olafyoorsen@hotmail.com>
2016-03-13 13:39:10 +0100

    index added setDefaultValues arg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-module.exports = function(options,defaults){
+module.exports = function(options,defaults,setDefaultValues){
     options = options || {};
+    setDefaultValues = setDefaultValues === undefined ? true : setDefaultValues;
     for(var option in options){
         if(defaults[option] === undefined){
             throw new Error('Invalid option "' + option + '". Available options: [' +
@@ -10,10 +11,18 @@ module.exports = function(options,defaults){
         }
     }
     var validated = {};
-    for(var option in defaults){
-        validated[option] = options[option] === undefined ?
-            defaults[option] :
-            options[option];
+    if(setDefaultValues){
+        for(var option in defaults){
+            validated[option] = options[option] === undefined ?
+                defaults[option] :
+                options[option];
+        }
+    } else {
+        for(var option in options){
+            validated[option] = options[option];
+        }
     }
+
+
     return validated;
 };
